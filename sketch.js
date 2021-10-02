2dc6990ff4dffaf6127e5ca1a432b13b6e160012
arshith787 <82289984+arshith787@users.noreply.github.com>
2021-10-02 14:08:07 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -20,17 +20,17 @@ function draw() {
   if(wall.x - car.x < (car.width + wall.width)/2){
       car.velocityX = 0;
 
-    var deformation = (0.5 * wieght * speed * speed)/22500;
-    if(deformation < 80){
-      car.shapeColor = "green";
+    var deformation =0.5 * wieght * speed * speed/22500;
+    if(deformation > 180){
+      car.shapeColor =color(255,0,0);
     }
-    if(deformation > 80 && deformation < 180){
+    if(deformation < 180 && deformation > 100){
       car.shapeColor = color(230,230,0);
     }
-    if(deformation > 180){
-      car.shapeColor = "red";
+    if(deformation<100){
+      car.shapeColor =color(0,255,0;
     }
   }
-
+  
   drawSprites();
 }
